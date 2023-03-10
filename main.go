d0657dbf08bab91ed198f7beb2d5570386e56bb3
caran0va <cara@caranova.space>
2023-03-09 18:55:40 -0800

    added create command for tasklists and general help command

--- main.go
+++ main.go
@@ -63,7 +63,6 @@ func mainMenu() {
 	var selectedItem string = ""
 	var selectedIdx int = -1
 	var selector string = "  "
-	var err error
 menuloop:
 	for {
 		// reset input fields
@@ -73,67 +72,87 @@ menuloop:
 
 		fmt.Printf("\nPlease type a command> ")
 		scanner.Scan()
-		//TODO: add zero length commnd handling
-		command = strings.Fields(scanner.Text())
+		if scanner.Text() != "" {
+			command = strings.Fields(scanner.Text())
 
-		switch {
-		case strings.EqualFold(command[0], "help"):
-
-		case strings.EqualFold(command[0], "create"):
 			switch {
-			case strings.EqualFold(command[1], "task") && selectedItem == "Task List" && selectedIdx != -1:
-				fmt.Printf("Task Name: ")
-				scanner.Scan()
-				taskName := scanner.Text()
-				fmt.Printf("Task Description: ")
-				scanner.Scan()
-				taskDescription := scanner.Text()
-				taskBook[selectedIdx].newTask(taskName, taskDescription)
-				fmt.Printf("Task %s Created", taskName)
+			default:
+				fmt.Println()
+				greeting()
+				fmt.Println()
+				fmt.Println("HELP - Display list of available commands.")
+				fmt.Println("CREATE -  Create a tasklist or task.")
+				fmt.Println("LIST -  Display a list of objects.")
+				fmt.Println("DELETE - Unimplemented at this time.")
+				fmt.Println("SELECT - Shift focus to an object.")
+				fmt.Println("SELECTED -  Display the selected object.")
+			case strings.EqualFold(command[0], "create") && len(command) == 2:
+				switch {
+				case strings.EqualFold(command[1], "task") && selectedItem == "Task List" && selectedIdx != -1:
+					fmt.Printf("Task Name: ")
+					scanner.Scan()
+					taskName := scanner.Text()
+					fmt.Printf("Task Description: ")
+					scanner.Scan()
+					taskDescription := scanner.Text()
+					taskBook[selectedIdx].newTask(taskName, taskDescription)
+					fmt.Printf("Task %s Created", taskName)
+				case strings.EqualFold(command[1], "tasklist"):
+					fmt.Printf("Task  List Owner: ")
+					scanner.Scan()
+					taskListOwner := scanner.Text()
+					fmt.Printf("Task List Title: ")
+					scanner.Scan()
+					taskListName := scanner.Text()
+					taskBook = append(taskBook, *newTaskList(taskListName, taskListOwner))
+					fmt.Printf("Task List %s Created for %s", taskListName, taskListOwner)
+				}
 
-			}
-
-		case strings.EqualFold(command[0], "list"):
-			switch {
-			case strings.EqualFold(command[1], "tasklist"):
-				fmt.Printf("%-20v %-40v %-20v %v", "\n  Task List ###:", "Title:", "Owner:", "# of Tasks:\n\n")
-				for i, tl := range taskBook {
-					if selectedItem == "Task List" && selectedIdx == i {
-						selector = "* "
-					} else {
+			case strings.EqualFold(command[0], "list") && len(command) == 2:
+				switch {
+				case strings.EqualFold(command[1], "tasklist"):
+					fmt.Printf("%-20v %-40v %-20v %v", "\n  Task List ###:", "Title:", "Owner:", "# of Tasks:\n\n")
+					for i, tl := range taskBook {
+						if selectedItem == "Task List" && selectedIdx == i {
+							selector = "* "
+						} else {
+							selector = "  "
+						}
+						fmt.Printf("%-20v%-40v %-20v %v\n", selector+"Tasklist "+strconv.Itoa(i), tl.title, tl.owner, len(tl.tasks))
 						selector = "  "
 					}
-					fmt.Printf("%-20v%-40v %-20v %v\n", selector+"Tasklist "+strconv.Itoa(i), tl.title, tl.owner, len(tl.tasks))
-					selector = "  "
-				}
-			case strings.EqualFold(command[1], "task"):
-				fmt.Printf("%-20v %-40v %v", "\n  Task ###:", "Title:", "Description:\n\n\n")
-				for i, task := range taskBook[selectedIdx].tasks {
-					fmt.Printf("%-20v%-40v %-20v\n\n", selector+"Task "+strconv.Itoa(i), task.title, task.descripion)
-				}
+				case strings.EqualFold(command[1], "task"):
+					fmt.Printf("%-20v %-40v %v", "\n  Task ###:", "Title:", "Description:\n\n\n")
+					for i, task := range taskBook[selectedIdx].tasks {
+						fmt.Printf("%-20v%-40v %-20v\n\n", selector+"Task "+strconv.Itoa(i), task.title, task.descripion)
+					}
 
-			}
-		case strings.EqualFold(command[0], "select"):
-			switch {
-			case strings.EqualFold(command[1], "tasklist"):
-				selectedItem = "Task List"
-				if selectedIdx, err = strconv.Atoi(command[2]); selectedIdx >= 0 {
-					fmt.Printf("Task List %d is the selected %s\n", selectedIdx, selectedItem)
+				}
+			case strings.EqualFold(command[0], "select") && len(command) == 3:
+				switch {
+				case strings.EqualFold(command[1], "tasklist"):
+					selectedItem = "Task List"
+					if arg2, err := strconv.Atoi(command[2]); err == nil && arg2 >= 0 && arg2 <= len(taskBook)-1 {
+						selectedIdx = arg2
+						fmt.Printf("Task List %d is the selected %s\n", selectedIdx, selectedItem)
+					} else if arg2 > len(taskBook)-1 {
+						fmt.Printf("Task List %d does not exist, please select a valid index", arg2)
+					} else {
+						fmt.Printf("Error occured: %v", err)
+					}
+
+				}
+			case strings.EqualFold(command[0], "delete"):
+				// TODO implement deleting thingies
+			case strings.EqualFold(command[0], "exit") && len(command) == 1:
+				break menuloop
+			case strings.EqualFold(command[0], "selected") && len(command) == 1:
+				if selectedIdx >= 0 && selectedItem != "" {
+					fmt.Printf("%s %d is currently selected\n", selectedItem, selectedIdx)
 				} else {
-					fmt.Printf("Error occured: %v", err)
+					fmt.Printf("Nothing is selected.")
 				}
-			}
-		case strings.EqualFold(command[0], "delete"):
-
-		case strings.EqualFold(command[0], "exit"):
-			break menuloop
-		case strings.EqualFold(command[0], "selected"):
-			if selectedIdx >= 0 && selectedItem != "" {
-				fmt.Printf("%s %d is currently selected\n", selectedItem, selectedIdx)
-			} else {
-				fmt.Printf("Nothing is selected.")
 			}
 		}
 	}
-
 }
